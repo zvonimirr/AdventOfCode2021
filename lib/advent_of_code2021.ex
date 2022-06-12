8fd6597d2d9ea0461320dbcb200d0077073f0a57
Zvonimir Rudinski <zvonimirurdinski@protonmail.ch>
2022-06-12 14:58:08 +0200

    Day 1: Part 2

--- lib/advent_of_code2021.ex
+++ lib/advent_of_code2021.ex
@@ -11,4 +11,17 @@ defmodule AdventOfCode2021 do
 
     AdventOfCode2021.Day1.solve(input)
   end
+
+  def day1_part2 do
+    input =
+      File.read!("inputs/day1.txt")
+      |> String.split("\n")
+      |> List.delete_at(-1)
+      |> Enum.map(fn number ->
+        Integer.parse(number)
+        |> Kernel.elem(0)
+      end)
+
+    AdventOfCode2021.Day1.solve_part2(input)
+  end
 end
--- lib/days/day1.ex
+++ lib/days/day1.ex
@@ -4,4 +4,12 @@ defmodule AdventOfCode2021.Day1 do
     |> Enum.chunk_every(2, 1, :discard)
     |> Enum.count(fn [a, b] -> b > a end)
   end
+
+  def solve_part2(input) do
+    input
+    |> Enum.chunk_every(3, 1, :discard)
+    |> Enum.map(&Enum.sum/1)
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.count(fn [a, b] -> b > a end)
+  end
 end
